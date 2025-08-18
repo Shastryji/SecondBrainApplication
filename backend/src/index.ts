4ff879be6041b73e3631a2d27bc40a47ef56b0b8
Shastryji <shivam1234shastry@gmail.com>
2025-08-18 21:08:31 +0530

    addin first route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,14 @@
 import express from "express"
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 
+app.use(express.json());
+
+app.post("/api/v1/sinup", async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+})
